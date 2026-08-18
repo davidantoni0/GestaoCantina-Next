c1ed614fc6ab2c4c3a1d98e802c6d11636352dfa
Silas Pereira Martins <pet0033648@aluno.firjansenaisesi.com.br>
2026-08-18 20:21:05 -0300

    Novas ãlterações no page.tsx

--- app/MainPage/page.tsx
+++ app/MainPage/page.tsx
@@ -2,11 +2,10 @@ import { CardProdutos } from "../components/CardProdutos"
 
 export default function HomePage () {
     return (
-        <main className="bg-gray-600"> 
+        <main className="bg-blue-800 text-center font-bold p-2 m-2 w-fit rounded-2xl"> 
             <h1> Cantina SENAI </h1>
             <CardProdutos/>
         </main>
-
 
     )
 }
